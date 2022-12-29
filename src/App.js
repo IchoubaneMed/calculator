3834a184cc5763266e88bb602a82232ff802e9ad
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-12-29 22:00:04 +0100

    Created the Calculator content

--- src/App.js
+++ src/App.js
@@ -3,7 +3,38 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <div className="calculator">
+        <div className="screen">
+          <div className="formula"></div>
+          <div className="display"></div>
+        </div>
+        <div className="buttons">
+          <div className="operators">
+            <button className="operators_btn" id="add">+</button>
+            <button className="operators_btn" id="substract">-</button>
+            <button className="operators_btn" id="multiply">*</button>
+            <button className="operators_btn" id="divide">/</button>
+            <button className="operators_btn" id="clear">C</button>
+          </div>
+          <div className="digits">
+            <button className="digits_btn" id="seven">7</button>
+            <button className="digits_btn" id="eight">8</button>
+            <button className="digits_btn" id="nine">9</button>
+
+            <button className="digits_btn" id="four">4</button>
+            <button className="digits_btn" id="five">5</button>
+            <button className="digits_btn" id="six">6</button>
+            
+            <button className="digits_btn" id="one">1</button>
+            <button className="digits_btn" id="two">2</button>
+            <button className="digits_btn" id="three">3</button>
+
+            <button className="digits_btn" id="zero">0</button>
+            <button className="digits_btn" id="decimal">.</button>
+            <button className="digits_btn" id="equal">=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
